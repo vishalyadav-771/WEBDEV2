9e3954f931b5cfaecb9f459f21ed1e1e2ae0eeb8
vishalyadav-771 <vy309908@gmail.com>
2026-06-02 13:23:34 +0530

    Digital Clock

---
+++ digitalClock/script.js
@@ -0,0 +1,7 @@
+function updateClock(){
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    document.getElementById('clock').textContent = time;
+}
+setInterval(updateClock, 1000);
+updateClock();
